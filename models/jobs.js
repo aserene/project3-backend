1bef11917ea7685df1a2bcf8142b02e1a25d59a7
Athena Serene <aserenehoosang@gmail.com>
2023-01-21 08:06:44 -0800

    updated markdowon, added diagram png, updated job schema

--- models/jobs.js
+++ models/jobs.js
@@ -3,6 +3,7 @@ const mongoose = require("./connection")
 const JobSchema = new mongoose.Schema({
     companyName: String,
     jobTitle: String,
+    location: String,
     salary: Number,
     link: String,
     comments: String
