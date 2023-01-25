330254b90b6ac36f8e6ffb50c85cf9aa8727af3f
Athena Serene <aserenehoosang@gmail.com>
2023-01-24 16:59:54 -0800

    removed session based auth codes and replaced with jwt codes. only half working

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,9 @@
+require("dotenv").config()
 const express = require("express")
 const User = require("../models/user")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const {SECRET} = process.env
 
 const router = express.Router()
 
@@ -11,49 +14,39 @@ router.get("/", (req, res) => {
 router.get("/signup", (req, res) => {
     res.send("this will be the signup page")
 })
-// the below post route grabs what will be in the password field, encrypts it, and then creates the user.
 
-// router.post("/signup", async (req, res) => {
-//     req.body.password = await bcrypt.hash(req.body.password, await bcrypt.genSalt(10));
-//     User.create(req.body, (err, user) => {
-//         res.redirect("/user/login")
-//     })
-// })
+router.post("/signup", async (req, res) => {
+    try {
+        req.body.password = await bcrypt.hash(req.body.password, 10)
+        const newUser = await User.create(req.body)
+        res.status(200).json(newUser)
+    } catch (error) {
+        res.status(400).json({error})
+    }
+})
 
 router.get("/login", (req, res) => {
     res.send("this will be the default landing page where the user will be prompted to login. there will be a link to go to a sign up page for new users")
 })
 
-//the below post route will let the user login
-
-// router.post("/login", (req, res) => {
-//     // res.send("login")
-//     // get the data from the request body
-//     const { username, password } = req.body;
-//     User.findOne({ username }, (err, user) => {
-//       // checking if userexists
-//       if (!user) {
-//         res.send("user doesn't exist");
-//       } else {
-//         //check if password matches
-//         const result = bcrypt.compareSync(password, user.password);
-//         if (result) {
-//             req.session.username = username;
-//             req.session.loggedIn = true ;
-//           res.redirect("/wish");
-//         } else {
-//           res.send("wrong password");
-//         }
-//       }
-//     })
-// })
-
-//the route below will let the user sign out
-
-// router.get("/logout", (req, res) => {
-//     // destroy session and redirect to main page
-//     req.session.destroy((err) => {
-//         res.redirect("/")
-//     })
-// })
+router.post("/login", async (req, res) => {
+    try {
+        const {username, password} = req.body
+        const user = await User.findOne({username})
+        if (user){
+            const match = await bcrypt.compare(password, user.password)
+            if(match){
+                const token = await jwt.sign({username}, SECRET)
+                res.status(200).json({token})
+            } else {
+                res.status(400).json({error: "PASSWORD IS INCORRECT OR DOES NOT EXIST"})
+            }
+        }else {
+            res.status(400).json({error: "USER DOES NOT EXIST"})
+        }
+    } catch (error) {
+        res.status(400).json({error})
+    }
+})
+// the logout function would usually be here but will instead go on the frontend
 module.exports = router
